8046191386e517a4263e93e96555b02006e4876e
attaln <attaln@ISSAATTALNL7C.corp.emc.com>
2016-12-06 18:37:56 +0200

    aa

---
+++ CouponSystemWeb/WebContent/scripts/company.js
@@ -0,0 +1,91 @@
+(function () {
+    var app = angular.module("couponSystem");
+
+    var companyController = function ($scope, $http, uiGridConstants, $uibModal, $confirm) {
+
+        $http.get("rest/company/getCompanyName")
+            .then(function (response) {
+                $scope.companyName = response.data;
+            });
+        $scope.mySelectedRow = false;
+        $scope.deleteCoupon = function () {
+            $confirm({
+                text: 'Are you sure you want to delete "' + $scope.mySelectedRow.CouponTitle + ' (ID: ' + $scope.mySelectedRow.CouponId + ')" ?',
+                title: 'Delete it',
+                ok: 'Yes',
+                cancel: 'No'
+            })
+                .then(function () {
+                    $http.delete("rest/company/deleteCoupon?couponId=" + $scope.mySelectedRow.CouponId)
+                        .then(function () {
+                            $scope.getCoupons();
+                        });
+
+                });
+        };
+
+        $scope.newCoupon = function () {
+            $scope.editCoupon(null, true);
+        };
+
+        $scope.editCoupon = function (row, isNew) {
+            var modalInstance = $uibModal.open({
+                controller: "couponDialog",
+                templateUrl: 'html/coupondialog.html',
+                resolve: {
+                    selectedRow: () => {
+                        if (row) return row.entity
+                    },
+                    isNew: () => isNew,
+                    getCoupons: () => $scope.getCoupons
+                }
+            })
+        };
+
+        $scope.editCouponButton = function () {
+            $scope.editCoupon({entity: $scope.mySelectedRow});
+        };
+
+        $scope.companyCoupons = {
+            onRegisterApi: function (gridApi) {
+                $scope.gridApi = gridApi;
+                gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                    $scope.mySelectedRow = gridApi.selection.getSelectedRows()[0];
+                });
+            },
+            enableRowSelection: true,
+            multiSelect: false,
+            enableSelectAll: false,
+            enableRowHeaderSelection: false,
+            enableFiltering: true,
+            rowTemplate: "<div ng-dblclick=\"grid.appScope.editCoupon(row)\" ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell></div>",
+            columnDefs: [
+                {name: 'CouponId', type: 'number', sort: {direction: uiGridConstants.ASC, priority: 0}},
+                {name: 'CouponTitle'},
+                {name: 'CouponStartDate', type: 'date', cellFilter: 'date:\'yyyy-MM-dd\''},
+                {name: 'CouponEndDate', type: 'date', cellFilter: 'date:\'yyyy-MM-dd\''},
+                {name: 'CouponAmount', type: 'number'},
+                {name: 'CouponType'},
+                {name: 'CouponMessage'},
+                {name: 'CouponPrice', type: 'number', cellFilter: 'number: 2'},
+                {
+                    name: 'CouponImagePath',
+                    displayName: 'Image',
+                    cellTemplate: "<img height=\"30px\" ng-src=\"{{grid.getCellValue(row, col)}}\" lazy-src>"
+                }
+            ]
+        };
+
+        $scope.getCoupons = function () {
+            $http.get("rest/company/getAllCoupons")
+                .then(function (response) {
+                    $scope.companyCoupons.data = response.data;
+                });
+        };
+
+        $scope.getCoupons();
+
+    }
+
+    app.controller("companyController", companyController);
+}());
---
+++ CouponSystemWeb/src/com/netanel/coupons/web/filter/LoginFilter.java
@@ -0,0 +1,63 @@
+package com.netanel.coupons.web.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+//@WebFilter(urlPatterns = { "/LoginFilter" }, servletNames = { "Jersey REST Service" })
+public class LoginFilter implements Filter {
+
+    public LoginFilter() {
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//TODO: remove system outs
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		//String url = req.getContextPath() + "/index.html";
+		HttpSession session = req.getSession(false);
+		
+		
+		if (session == null) {
+			filterOutput(res, "No Session Found! <br> Redirecting to Login Page.");
+			return;
+		} else if (session.getAttribute("FACADE") == null){
+			filterOutput(res, "No Facade in Session! <br> Redirecting to Login Page.");
+			return;
+		} else {
+			
+			System.out.println(session.getAttribute("FACADE").getClass().getSimpleName() + " found in session!");
+		}
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	private void filterOutput(HttpServletResponse res, String str) throws IOException {
+		res.setContentType("application/json");
+		PrintWriter out = res.getWriter();
+		
+		System.out.println(str);
+		
+		res.setStatus(500);
+		out.print("{\"filter\": \""+ str +"\", \"redirect\": true}");
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
